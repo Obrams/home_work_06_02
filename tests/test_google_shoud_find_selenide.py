989f0c6cfd7e563c1a4aec1bb176da0a968d9af0
NOzerov <testirochik@gmail.com>
2023-06-06 18:42:06 +0300

    Написание позитивного и негативного теста для 2 домашнего задания 6 потока.

---
+++ tests/test_google_shoud_find_selenide.py
@@ -0,0 +1,22 @@
+import pytest
+from selene import browser, have, be
+
+
+@pytest.fixture
+def browser_size_window():
+    browser.config.window_width = 1360
+    browser.config.window_height = 768
+
+
+def test_successful_find_selene(browser_size_window):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
+    browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
+
+
+def test_unsuccessful_find_selene(browser_size_window):
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('asldksal;dkas;dlka;sldka;sdk;lsadkas;ld').press_enter()
+    assert browser.element('[id="result-stats"]').should(
+        have.text('Результатов: примерно 0')) != browser.element('[id="result-stats"]').should(
+        have.text('Результатов: примерно 0')), "Found a search result that shouldn't be there"
