0ee83dd54bbc090e9ffa09259e0f91d9b59ef68e
NOzerov <testirochik@gmail.com>
2023-06-07 09:11:50 +0300

    Добавил фикстуру open_browser_link

--- tests/test_google_shoud_find_selenide.py
+++ tests/test_google_shoud_find_selenide.py
@@ -8,13 +8,16 @@ def browser_size_window():
     browser.config.window_height = 768
 
 
-def test_successful_find_selene(browser_size_window):
+@pytest.fixture
+def open_browser_link():
     browser.open('https://google.com')
+
+
+def test_successful_find_selene(open_browser_link, browser_size_window):
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
     browser.element('[id="search"]').should(have.text('User-oriented Web UI browser tests in Python'))
 
 
-def test_unsuccessful_find_selene(browser_size_window):
-    browser.open('https://google.com')
+def test_unsuccessful_find_selene(open_browser_link, browser_size_window):
     browser.element('[name="q"]').should(be.blank).type('asldksal;dkas;dlka;sldka;sdk;lsadkas;ld').press_enter()
     assert browser.element('[id="result-stats"]').should(have.text('Результатов: примерно 0'))
